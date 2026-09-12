7de26c3c9bcc1ec37584328f225ab0388b6cb289
Slamifoe21 <akabovm2@gmail.com>
2026-09-13 00:08:50 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -ldflags="-s -w" -trimpath -o ./output/xd-linux
+go build -ldflags="-s -w -X main.version=1.0.0 -X main.commit=$(git rev-parse --short HEAD 2>/dev/null || echo none)" -trimpath -o ./output/xd-linux
